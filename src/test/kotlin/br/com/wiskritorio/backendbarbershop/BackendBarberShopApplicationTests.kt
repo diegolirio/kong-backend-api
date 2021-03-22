d6f52c09b29bc36b5a40d56c0c035644c54a728d
Diego Lirio <diegolirio.dl@gmail.com>
2021-03-21 21:48:36 -0300

    First Commit

---
+++ src/test/kotlin/br/com/wiskritorio/backendbarbershop/BackendBarberShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.wiskritorio.backendbarbershop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendBarberShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
